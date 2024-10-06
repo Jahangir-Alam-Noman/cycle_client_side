520239339beef7d89f37ffb9489bc53b13a6b0f9
Jahangir-Alam-Noman <jahangir2km@gmail.com>
2024-10-07 01:33:41 +0600

    firtebase tested

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,18 +2,20 @@
 
 import React from "react";
 import "./Login.css";
-import Button from "react-bootstrap/Button";
 import useAuth from "../../../hooks/useAuth";
+import { Alert } from "react-bootstrap";
 
 const Login = () => {
-  const { signInWithGoogle } = useAuth();
-
+  const { user, authError, signInWithGoogle } = useAuth();
+  // console.log(user);
   return (
     <div>
       <h2>I'm From Login Page</h2>
-      <button onClick={signInWithGoogle} type='submit'>
-        Sign In
+      <button onClick={signInWithGoogle} type='button' className='btn btn-info'>
+        Sign In With Google
       </button>
+      {user?.email && <Alert variant='success'>{user.email}</Alert>}
+      <p className='text-danger fw-bolder'>{authError}</p>
     </div>
   );
 };
